3dbe366281cf6bbafbe4f914dc8479e4bbc29479
sarim2000 <sarimbleedblue@gmail.com>
2021-05-27 11:17:35 +0530

    used withRouter to route page to different links

---
+++ README.md
@@ -0,0 +1,3 @@
+### E-Commerce App using React
+
+Learning about React, art of breaking down websites into smaller components which makes it more flexible to use.
--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import { Route, Switch } from "react-router";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 
+const HatsPage = () => (
+	<div>
+		<h1>HATS PAGE</h1>
+	</div>
+);
+
 function App() {
-	return <HomePage />;
+	return (
+		<div>
+			<Switch>
+				<Route exact path="/" component={HomePage} />
+				<Route exact path="/hats" component={HatsPage} />
+			</Switch>
+		</div>
+	);
 }
 
 export default App;
