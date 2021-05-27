2b65386581bfcd1f7fd9fde7c4c3e4dc33898e0c
sarim2000 <sarimbleedblue@gmail.com>
2021-05-27 09:10:38 +0530

    broken our homepage into smaller components and have added stylings

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,9 @@
+import "./homepage.styles.scss"
+import Directory from "../../component/directory/directory.component"
+const HomePage = () => (
+    <div className="homepage">
+        <Directory/>
+    </div>
+)
+
+export default HomePage;
