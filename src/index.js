4addad883e6e509c3c7018d39659cfe33b9d04f6
Sarim Ahmed <50093514+sarim2000@users.noreply.github.com>
2021-06-02 14:48:28 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,9 +4,14 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 
+import { Provider } from "react-redux";
+
 ReactDOM.render(
-	<BrowserRouter>
-		<App />
-	</BrowserRouter>,
+	<Provider>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</Provider>,
+
 	document.getElementById("root")
 );
