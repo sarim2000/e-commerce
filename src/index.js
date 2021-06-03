983738682d803fd459afa668ea3cd8a2f9adddcf
Sarim Ahmed <50093514+sarim2000@users.noreply.github.com>
2021-06-03 12:59:16 +0530

    learning redux

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import "./index.css";
 import App from "./App";
 
 import { Provider } from "react-redux";
-
+import store from "./redux/store";
 ReactDOM.render(
-	<Provider>
+	<Provider store={store}>
 		<BrowserRouter>
 			<App />
 		</BrowserRouter>
